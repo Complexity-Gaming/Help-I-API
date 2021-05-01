11f5d3648a01ca96e9da00a0db45568bf603156f
DiegoJohnsonL <diegojohnson46@gmail.com>
2021-05-01 16:40:41 -0500

    Create Extensions

---
+++ Help-I.API/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Help_I.API.Extensions
+{
+    public class ModelStateExtensions
+    {
+    }
+}
